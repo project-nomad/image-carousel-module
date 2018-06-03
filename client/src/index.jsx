d0e9c0c830ac4028f5418ca8d228c2c038d2060b
Daryl Lin <daryllin@Daryls-MacBook-Pro.local>
2018-06-02 18:19:12 -0700

    Implemented helper functions to create mock data: Loaded mock data into database.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,19 @@
- console.log('hello')
+const React = require('react');
+
+const reactDOM = require('react-dom');
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div>Hello world</div>
+    );
+  }
+}
+
+reactDOM.render(<App />, document.getElementById('App'));
