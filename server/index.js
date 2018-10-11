2d7e6c9afd0cd9af1a7e50b95124fe7637f8f71f
Daryl Lin <daryllin5@gmail.com>
2018-10-10 17:25:26 -0700

    fixed process

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
 
-app.listen((process.env.PORT || 8080), () => {
-  console.log('Example App listening on port 3005');
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Example App listening on port 8080');
 });
